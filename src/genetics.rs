34c8a33ce469b0fffb086ab8b81c245b86ceb576
Nicola <nicola.altobel@studenti.unitn.it>
2026-01-28 22:42:37 +0100

    Refactored Explorer struct and message handling

---
+++ src/genetics.rs
@@ -0,0 +1,8 @@
+use common_game::components::resource::{BasicResourceType, ComplexResourceType};
+use common_game::utils::ID;
+
+pub(crate) enum Intention{
+    Generate(Option<BasicResourceType>),
+    Combine(Option<ComplexResourceType>),
+    Move(Option<ID>)
+}
--- src/logging_utils.rs
+++ src/logging_utils.rs
@@ -1,6 +1,3 @@
-use crate::Explorer;
-use common_game::logging::{ActorType, Channel, EventType, LogEvent, Participant, Payload};
-use common_game::utils::ID;
 
 #[macro_export]
 macro_rules! payload {
@@ -11,29 +8,4 @@ macro_rules! payload {
         )*
         p
     }};
-}
-
-impl Explorer {
-    /// Creates and emits a log event without sender and receiver, and with `EventType::InternalOrchestratorAction`
-    pub fn log_internal(channel: Channel, payload: Payload) {
-        LogEvent::system(EventType::InternalExplorerAction, channel, payload).emit();
-    }
-
-    /// Creates a log event with itself as sender
-    pub fn log_msg_to(
-        &self,
-        channel: Channel,
-        event_type: EventType,
-        to: (ActorType, ID),
-        payload: Payload,
-    ) {
-        LogEvent::new(
-            Some(Participant::new(ActorType::Explorer, self.id)),
-            Some(Participant::new(to.0, to.1)),
-            event_type,
-            channel,
-            payload,
-        )
-        .emit();
-    }
 }
