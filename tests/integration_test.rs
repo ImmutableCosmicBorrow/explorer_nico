9da481e3949a1bf556226b77e19b5c3ff036ad3a
Nicola <nicola.altobel@studenti.unitn.it>
2026-01-29 10:36:01 +0100

    Fixed the sending of MovedToPlanetResult

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,13 +1,15 @@
-use std::thread;
-use std::time::Duration;
 use common_explorer::{ExplorerAI, ExplorerBagContent};
-use common_game::protocols::orchestrator_explorer::{ExplorerToOrchestrator, OrchestratorToExplorer};
+use common_game::protocols::orchestrator_explorer::{
+    ExplorerToOrchestrator, OrchestratorToExplorer,
+};
 use common_game::protocols::planet_explorer::{ExplorerToPlanet, PlanetToExplorer};
 use crossbeam_channel::unbounded;
 use explorer_nico::Explorer;
+use std::thread;
+use std::time::Duration;
 
 #[test]
-fn create_explorer(){
+fn create_explorer() {
     // 0. Channels
     let (tx_eto, rx_eto) = unbounded::<ExplorerToOrchestrator<ExplorerBagContent>>();
     let (_tx_etp, _rx_etp) = unbounded::<ExplorerToPlanet>();
@@ -15,13 +17,7 @@ fn create_explorer(){
     let (tx_ote, rx_ote) = unbounded::<OrchestratorToExplorer>();
 
     // 1. Create Explorer
-    let mut explorer = Explorer::new(
-        266,
-        tx_eto,
-        rx_ote,
-        rx_pte,
-        Duration::from_millis(500),
-    );
+    let mut explorer = Explorer::new(266, tx_eto, rx_ote, rx_pte, Duration::from_millis(500));
 
     // 2. Spawn thread
     let handle = thread::spawn(move || {
@@ -29,19 +25,25 @@ fn create_explorer(){
     });
 
     // 3. Send Start
-    tx_ote.send(OrchestratorToExplorer::StartExplorerAI).expect("Error while sending to the Explorer");
+    tx_ote
+        .send(OrchestratorToExplorer::StartExplorerAI)
+        .expect("Error while sending to the Explorer");
 
     // 4. Check response
     let response = rx_eto.recv().expect("Error while receiving from Explorer");
-    let expected : ExplorerToOrchestrator<ExplorerBagContent> = ExplorerToOrchestrator::StartExplorerAIResult {explorer_id : 266};
+    let expected: ExplorerToOrchestrator<ExplorerBagContent> =
+        ExplorerToOrchestrator::StartExplorerAIResult { explorer_id: 266 };
     assert_eq!(format!("{response:?}"), format!("{expected:?}"));
 
     // 4. Send Kill
-    tx_ote.send(OrchestratorToExplorer::KillExplorer).expect("Error while sending to the Explorer");
+    tx_ote
+        .send(OrchestratorToExplorer::KillExplorer)
+        .expect("Error while sending to the Explorer");
 
     // 5. Check response
     let response = rx_eto.recv().expect("Error while receiving from Explorer");
-    let expected : ExplorerToOrchestrator<ExplorerBagContent> = ExplorerToOrchestrator::KillExplorerResult {explorer_id : 266};
+    let expected: ExplorerToOrchestrator<ExplorerBagContent> =
+        ExplorerToOrchestrator::KillExplorerResult { explorer_id: 266 };
     assert_eq!(format!("{response:?}"), format!("{expected:?}"));
 
     // Last. Join thread
